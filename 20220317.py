c17919f0fe72d33d16a9f308127704395e8b23eb
subinlee <spinney1012@naver.com>
2022-03-17 18:42:08 +0900

    first commit

---
+++ 20220317.py
@@ -0,0 +1,36 @@
+#1
+print("Hello World")
+#2
+print("Mary's cosmetics")
+#3
+print('신씨가 소리질렀다. "도둑이야"')
+#4
+print("C:\Windows")
+#5
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+#51
+movie_rank=["닥터 스트레인지", "스플릿", "럭키"]
+print(movie_rank)
+#52
+movie_rank.append("배트맨")
+print(movie_rank)
+#53
+movie_rank.insert(1,'슈퍼맨')
+print(movie_rank)
+#58
+nums=[1, 2, 3, 4, 5]
+print(sum(nums))
+#59
+cook=["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+print(len(cook))
+#60
+print(sum(nums)/len(nums))
+#102
+print(3 == 5)
+#103
+print(3 < 5)
+#104
+x = 4
+print(1 < x < 5)
+#105
+print((3 == 3) and (4 != 3))
